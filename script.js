c89f16617f8ef01dadf118985b381561cd155c1a
marcomuna <marco.muna143@gmail.com>
2026-03-28 14:18:28 +0530

    Updating for PWA

--- script.js
+++ script.js
@@ -54,3 +54,13 @@ audioBtn.addEventListener("click", () => {
   audio.play();
   console.log(audioUrl);
 });
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("Service Worker Registered"))
+      .catch((err) => console.log("Error:", err));
+  });
+}
